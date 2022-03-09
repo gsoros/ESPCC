0954a65aac24dfe8e291a7d16247106e33c8e130
G Soros <gsoros@users.noreply.github.com>
2022-03-09 22:50:30 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
-
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+#include <U8g2lib.h>
 
 SoftwareSerial ss;
 TinyGPSPlus gps;
+U8G2_SH1106_128X64_VCOMH0_F_HW_I2C oled(
+    U8G2_R1,
+    U8X8_PIN_NONE,
+    GPIO_NUM_18,
+    GPIO_NUM_5);
 
 void setup() {
     Serial.begin(115200);
     Serial.println("--------");
     ss.begin(9600, SWSERIAL_8N1, GPIO_NUM_17, GPIO_NUM_16);
+    oled.begin();
+    oled.setFont(u8g2_font_logisoso32_tn);
 }
 
 void loop() {
@@ -18,20 +25,43 @@ void loop() {
         gps.encode(c);
         // Serial.print(c);
     }
-    if (gps.location.isUpdated()) {
-        Serial.printf("%s Lat=%f Lon=%f Alt=%f HDOP=%d Speed=%f, Sat=%d%s SeFi=%d T=%d:%d:%d%s\n",
-                      gps.location.isValid() ? "fix" : "___",
-                      gps.location.lat(),  //
-                      gps.location.lng(),
-                      gps.altitude.meters(),
-                      gps.hdop.value(),
-                      gps.speed.kmph(),
-                      gps.satellites.value(),
-                      gps.satellites.isValid() ? "" : "X",
-                      gps.sentencesWithFix(),
-                      gps.time.hour(),
-                      gps.time.minute(),
-                      gps.time.second(),
-                      gps.time.isValid() ? "" : "X");
+    if (gps.location.isValid()) {
+        Serial.printf(
+            "%f %f %.1fm %.2fkm/h %d %d%ssat %02d:%02d:%02d%s\n",  //
+            gps.location.lat(),
+            gps.location.lng(),
+            gps.altitude.meters(),
+            gps.speed.kmph(),
+            gps.hdop.value(),
+            gps.satellites.value(),
+            gps.satellites.isValid() ? "" : "X",
+            gps.time.hour(),
+            gps.time.minute(),
+            gps.time.second(),
+            gps.time.isValid() ? "" : "X");
+
+        oled.clearBuffer();
+        oled.setCursor(0, 32);
+        oled.printf("%02d%d",
+                    gps.time.hour(),
+                    gps.time.minute() / 10);
+        oled.setCursor(0, 82);
+        oled.printf("%d%02d",
+                    gps.time.minute() % 10,
+                    gps.time.second());
+        oled.setCursor(0, 128);
+        oled.printf("%d%02d",
+                    gps.date.month() % 10,
+                    gps.date.day());
+        oled.sendBuffer();
+        return;
+    }
+    static uint32_t satellites = UINT32_MAX;
+    if (satellites != gps.satellites.value()) {
+        satellites = gps.satellites.value();
+        oled.clearBuffer();
+        oled.setCursor(0, 82);
+        oled.printf("%03d", satellites);
+        oled.sendBuffer();
     }
 }
