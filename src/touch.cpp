b4a6647b55bf8ecb933ae81f179ab3dca64ef73d
G Soros <gsoros@users.noreply.github.com>
2022-03-27 11:07:01 +0200

    .

--- src/touch.cpp
+++ src/touch.cpp
@@ -4,16 +4,23 @@
 void Touch::fireEvent(uint8_t index, Event event) {
     Atoll::Touch::fireEvent(index, event);
     board.oled.onTouchEvent(&pads[index], event);
-    if (event == Event::longTouch)
-        board.bleClient.startScan(5);
-    else if (event == Event::doubleTouch) {
-        for (uint8_t i = 0; i < board.bleClient.peersMax; i++) {
-            if (board.bleClient.peers[i] != nullptr) {
-                board.bleClient.peers[i]->disconnect();
-                board.bleClient.removePeer(board.bleClient.peers[i]);
-                board.bleClient.saveSettings();
-                break;
-            }
-        }
+    switch (event) {
+        case Event::longTouch: {
+            // board.bleClient.startScan(5);
+            board.recorder.start();
+        } break;
+        case Event::doubleTouch: {
+            // for (uint8_t i = 0; i < board.bleClient.peersMax; i++) {
+            //     if (board.bleClient.peers[i] != nullptr) {
+            //         board.bleClient.peers[i]->disconnect();
+            //         board.bleClient.removePeer(board.bleClient.peers[i]);
+            //         board.bleClient.saveSettings();
+            //         break;
+            //     }
+            // }
+            board.recorder.stop();
+        } break;
+        default:
+            break;
     }
 }
