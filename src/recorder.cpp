5b4497621d06cbf1d870bb0348d24ffd31f9e11d
G Soros <gsoros@users.noreply.github.com>
2023-03-04 00:12:32 +0100

    .

--- src/recorder.cpp
+++ src/recorder.cpp
@@ -1,6 +1,20 @@
 #include "board.h"
 #include "recorder.h"
 
+bool Recorder::start() {
+    bool res = Atoll::Recorder::start();
+    // log_d("start returned %d", res);
+    notifyStatus();
+    return res;
+}
+
+bool Recorder::stop(bool forgetLast) {
+    bool res = Atoll::Recorder::stop(forgetLast);
+    // log_d("stop returned %d", res);
+    notifyStatus();
+    return res;
+}
+
 void Recorder::onDistanceChanged(double value) {
     Atoll::Recorder::onDistanceChanged(value);
     board.display.onDistance((uint)value);
@@ -22,4 +36,18 @@ bool Recorder::rec2gpx(const char *in,
     if (res) board.webserver.generateIndex();
 #endif
     return res;
+}
+
+void Recorder::notifyStatus() {
+    BLECharacteristic *c = board.bleServer.getChar(BLEUUID(API_SERVICE_UUID), BLEUUID(API_TX_CHAR_UUID));
+    if (!c) {
+        log_e("could not get char");
+        return;
+    }
+    ApiMessage m = board.api.process("rec");
+    char reply[8 + strlen(m.reply)];
+    snprintf(reply, sizeof(reply), "%d;%d=%s", m.result->code, m.commandCode, m.reply);
+    log_d("notifying: %s", reply);
+    c->setValue((uint8_t *)reply, strlen(reply));
+    c->notify();
 }
--- src/recorder.h
+++ src/recorder.h
@@ -6,9 +6,12 @@
 
 class Recorder : public Atoll::Recorder {
    public:
+    virtual bool start() override;
+    virtual bool stop(bool forgetLast = false) override;
     virtual void onDistanceChanged(double value) override;
     virtual void onAltGainChanged(uint16_t value) override;
     virtual bool rec2gpx(const char *in, const char *out, bool overwrite = false) override;
+    virtual void notifyStatus();
 };
 
 #endif
